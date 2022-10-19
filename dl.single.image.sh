362ba6d2c25741566f14c4cb6281004bc942d17c
gigamicro <43441570+gigamicro@users.noreply.github.com>
2022-10-18 22:48:37 -0400

    move image extraction time to 50% rather than 10s

--- dl.single.image.sh
+++ dl.single.image.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-a=$1; youtube-dl "https://youtube.com/watch?v=$a" -i -f bestvideo --get-url | ffmpeg -hide_banner -i "$(cat)" -ss 10 -vframes 1 "./$a.png" || (
+a=$1; youtube-dl "https://youtube.com/watch?v=$a" -i -f bestvideo --get-url | \
+ffmpeg -hide_banner -i "$(cat)" -ss $(($(jq ".duration" <"./$a.json") / 2)) -vframes 1 "./$a.png" || (
 # echo Replacing with 1x1...
 # base64 -d <<< "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" > "./$a.png"
 echo no image!
