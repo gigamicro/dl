83346be2ba36c351f94f01de1e8d15416c1edc84
gigamicro <43441570+gigamicro@users.noreply.github.com>
2022-10-29 17:15:54 -0400

    keep more logs

--- dl.all.log.sh
+++ dl.all.log.sh
@@ -3,7 +3,7 @@ cd ~/Music
 pkill syncthing
 date +"%FT%T" >> ~/dl-times
 
-[ "$1" == "rm" ] && rm -rf dlc && echo "removed ./dlc"
+# [ "$1" == "rm" ] && rm -rf dlc && echo "removed ./dlc"
 rm -rf logs dlc.*.err
 (mkdir logs; mkdir logs/all) 2> /dev/null
 echo -n $$ > logs/pid
@@ -11,4 +11,4 @@ bash dl.c.main.sh 2>&1 | tee "logs/all/$(date +"%Y%m%dT%H%M%S.%N").log"
 rm logs/pid
 
 (syncthing -no-browser > /tmp/syncthing.log &) & > /dev/null
-(sleep 360; rm -d logs/dl* && rm -r logs)& >/dev/null
+# (sleep 360; rm -d logs/dl* && rm -r logs)& >/dev/null
