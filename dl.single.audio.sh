6f3b3907a671f8a37422d32d70b549ab9bfd690a
gigamicro <43441570+gigamicro@users.noreply.github.com>
2022-10-29 14:12:50 -0400

    redo a/v

--- dl.single.audio.sh
+++ dl.single.audio.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
-yt-dlp "https://youtube.com/watch?v=$1" -i -f bestaudio[acodec=opus] --get-url | ffmpeg -hide_banner -i "$(cat)" -vn -c:a copy "./$1.opus" || (
+ffmpeg -hide_banner \
+-i "$(yt-dlp "https://youtube.com/watch?v=$1" -f bestaudio[acodec=opus] --get-url)" \
+-vn \
+-c:a copy \
+"./$1.opus" || (
 echo Downloading non-opus original...
-yt-dlp "https://youtube.com/watch?v=$1" -i -f bestaudio --get-url | ffmpeg -hide_banner -i "$(cat)" -vn -c:a libopus "./$1.opus"
+ffmpeg -hide_banner \
+-i "$(yt-dlp "https://youtube.com/watch?v=$1" -f bestaudio --get-url)" \
+-vn \
+-c:a libopus \
+"./$1.opus"
 )
--- dl.single.image.sh
+++ dl.single.image.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
-a=$1; yt-dlp "https://youtube.com/watch?v=$a" -i -f bestvideo --get-url | \
-ffmpeg -hide_banner -i "$(cat)" -ss $(($(jq ".duration" <"./$a.json") / 2)) -vframes 1 "./$a.png" || (
-# echo Replacing with 1x1...
-# base64 -d <<< "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" > "./$a.png"
-echo no image!
-)
+ffmpeg -hide_banner \
+-i "$(yt-dlp "https://youtube.com/watch?v=$1" -f bestvideo --get-url)" \
+-ss $(($(jq ".duration" <"./$1.json") / 2)) \
+-vframes 1 \
+"./$1.png"
