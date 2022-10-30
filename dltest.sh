ce0f9f02cc3501a189202b973da8c4dc2c95589b
gigamicro <43441570+gigamicro@users.noreply.github.com>
2022-10-30 10:15:59 -0400

    remove extraneous scripts

--- dltest.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-mkdir /tmp/dltesting; cd /tmp/dltesting; touch q; rm ./*; ls ~/dl/dl/ | while read i; do touch "$i"; done; rm dl.m3u; cat ~/dl/dl/dl.m3u | while read i; do rm "$i"; done; ls|cat; rm -r /tmp/dltesting
