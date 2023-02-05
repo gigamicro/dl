1ceae4458850bbf2d55d8be8a6962e4ee40b2011
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-02-05 14:20:19 -0500

    ignore stuff

--- toignore.sh
+++ toignore.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cat maybe\ remove.m3u | \
 grep '\[...........]' | \
-sed 's/^Music\/yt\///; s/\/.*\[/\t/; s/].m4a//' | \
-while read i; do echo "youtube ${i#*       }" >> "/home/gigamicro/dl/ignore/${i%        *}.archive"; done
+sed 's/^Music\/dl\///; s/\/.*\[/\t/; s/].m4a//' | \
+while read i; do echo "youtube ${i#*	}" >> "./${i%	*}.archive"; done
