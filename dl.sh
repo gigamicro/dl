ae65ca1e05cd0a1933fb36898811e76b0a2c79b3
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-02-06 23:32:09 -0500

    change uploader check to survey
    artist check has no results

--- dl.sh
+++ dl.sh
@@ -9,11 +9,11 @@ while read listurl; do  if [ -z "$listurl" ]; then break; fi; (
   else
     name="$(yt-dlp "$listurl" --playlist-end 1 --flat-playlist --print playlist_title | \
       sed 's/^Album - //; s/ *(.*)$//; s/ *O[fficial riginal]*S[ound ]*T[rack]*$//i;  s/ *-.*$//;   s/^NA$//; s/^[SongVideo]*s$//')"
-    [ -z "$name" -o "$name" = 'NA' ] && echo 'getting artist'   && coverflag=y && echo "Singlet covers" && \
-    name="$(yt-dlp "$listurl" --playlist-end 1 --flat-playlist --print artist)"
-    [ -z "$name" -o "$name" = 'NA' ] && echo 'getting uploader' && \
-    name="$(yt-dlp "$listurl" --playlist-end 1 --flat-playlist --print uploader | \
-      sed 's/ - Topic$//')"
+    if [ -z "$name" -o "$name" = 'NA' ] then
+      echo 'getting uploader'
+      coverflag=y; echo "Singlet covers"
+      name="$(yt-dlp "$listurl" --flat-playlist --print uploader | sort | uniq -c | sort -nr | head -n 1 | tail -c +9 | sed 's/ - Topic$//')"
+    fi
   fi
   [ -z "$name" -o "$name" = 'NA' ] && echo 'invalid playlist name' && exit
   dir="$basedir/$name"
