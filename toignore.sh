d4f844b9a0f1f10feef19c2e69817b4ff9103937
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-03-02 15:26:36 -0500

    minor comment edit on toignore.sh

--- toignore.sh
+++ toignore.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# cat maybe\ remove.m3u | \
+# cat ~/Music/maybe\ remove.m3u | \
 grep '\[...........]' | \
 sed 's/^Music\/dl\///; s/\/.*\[/\t/; s/].m4a//' | \
 while read i; do echo "youtube ${i#*	}" >> "./${i%	*}.archive"; done
