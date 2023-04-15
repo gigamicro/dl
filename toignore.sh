882bfa340bce9392412ad3a20e0626144002af38
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-04-15 12:53:17 -0400

    remove mrp after ignoring contents

--- toignore.sh
+++ toignore.sh
@@ -4,3 +4,4 @@ cat ~/Music/maybe\ remove.m3u | \
 grep '\[...........]' | \
 sed 's/^Music\/dl\///; s/\/.*\[/\t/; s/].m4a//' | \
 while read i; do echo "youtube ${i#*	}" | tee "./${i%	*}.archive" ;echo '	->';echo "./${i%	*}.archive"; done
+rm ~/Music/maybe\ remove.m3u
