107ba4ffe3144ab75abe963ff83da7e1dc98baf3
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-05-06 23:21:05 -0400

    add track number to album lister

--- playlistalbums.sh
+++ playlistalbums.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sed 's/ .*//' <~/dl/playlists.m3u | tee /dev/fd/2 | while read i; do yt-dlp "$i" --print album | sort | uniq -c | sort -nr && echo ^-"$i" &; done
+sed 's/ .*//' <~/dl/playlists.m3u | tee /dev/fd/2 | \
+while read i; do yt-dlp "$i" --print album | sort | uniq -c | sort -nr && echo ^-"$i" & done
+# while read i; do yt-dlp "$i" --print '%(album)s/%(track_number)s' && echo ^-"$i" ; done
