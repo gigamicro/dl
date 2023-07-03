9f77cddafcbb3f0e478488912658dc682b13d0f1
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-07-03 15:44:40 -0400

    change trailing line gen in `toplaylists.sh` from echo to a\

--- toplaylists.sh
+++ toplaylists.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "$(xclip -sel p -o | sed 's/www.//; s/music.//; s/watch?v=[^&]*&/playlist?/')" >> ~/dl/playlists.m3u
+xclip -sel p -o | sed 's/www.//; s/music.//; s/watch?v=[^&]*&/playlist?/;a\' >> ~/dl/playlists.m3u
