11336b8c8a40cb185369742c7133fd25cc0f192e
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-07-10 16:24:51 -0400

    standardize check names

--- faVarchivecheck.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-ls ~/Music/local/dlarchive/ | grep -o '\-...........\.' | sed 's/^-//; s/\.$//' | while read -r i; do grep "\.$i"<~/Music/dl/faV/faV.m3u; done
--- notinm3u.faV.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mkdir /tmp/dltesting
-cd /tmp/dltesting
-touch q; rm ./*
-ls ~/Music/dl/faV/ | while read i; do touch "$i"; done
-rm *.m3u
-cat ~/Music/dl/faV/faV.m3u | grep -F '].' | while read i; do rm "$i"; done
-ls
-rm -r /tmp/dltesting
