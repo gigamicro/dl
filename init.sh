3e40ae55ea2a41e9f6e7ec8f75e219cf5caaa3ec
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-07-10 19:59:43 -0400

    add tags to init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
 scriptdir="$(dirname "$0")"
+echo ===untrash===
 "$scriptdir/untrash.sh"
+echo ===toignore===
 "$scriptdir/toignore.sh"
 if [ "$1" = "z" ]; then
-	"$scriptdir/covercheck.sh" | while read -r i; do 
+	echo ===covercheck===
+	"$scriptdir/covercheck.sh" | while read -r i; do
 		case $i in
 		r*) true ;;
 		m*) rm -v "${i#*: }" ;;
@@ -11,10 +14,14 @@ if [ "$1" = "z" ]; then
 		esac
 	done
 fi
+echo ===dl===
 "$scriptdir/dl.sh"
+echo ===m3ucheck===
 "$scriptdir/m3ucheck.sh" | "$scriptdir/toarchive.sh"
 if [ "$1" = "z" ]; then
+	echo ===archivecheck===
 	"$scriptdir/archivecheck.sh" | while read -r i; do rm -v "$i"; done
+	echo ===faVduplicatecheck===
 	"$scriptdir/faVduplicatecheck.sh" | grep -o '\[[a-zA-Z0-9_-]\{11\}\]' | while read -r i; do
 		sed -e "/$i$/ s/^#*/#/" -i "$scriptdir/faV.m3u" && rm -v "$(cat "$scriptdir/basedir")/faV/$i"
 	done
