0a1550ead352ba2278ec32eecb7ade8f56a20454
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-07-10 20:20:23 -0400

    minor

--- toignore.sh
+++ toignore.sh
@@ -7,5 +7,8 @@ cd "$scriptdir/ignore/" || exit
 cat ~/Music/maybe\ remove.m3u | \
 grep '\[...........]' | \
 sed 's/^Music\/dl\///; s/\/.*\[/\t/; s/].m4a//' | \
-while read -r i; do echo "youtube ${i#*	}" | tee /dev/fd/2 >>"./${i%	*}.archive";echo "	\\-> ./${i%	*}.archive" >/dev/fd/2; done
+while read -r i; do
+	echo "youtube ${i#*	}" | tee /dev/fd/2 >>"./${i%	*}.archive"
+	echo "	\\-> ./${i%	*}.archive" >/dev/fd/2
+done
 rm ~/Music/maybe\ remove.m3u
