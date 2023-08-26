929e36e8fe46b75dc2a897b527cfa453bdf1a5de
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-08-26 18:48:39 -0400

    use 'cut' in archivecheck

--- archivecheck.sh
+++ archivecheck.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 {
-find "$(cat "$(dirname "$0")/archivedir")" -type f | grep -o '[[-][a-zA-Z0-9_-]\{11\}[].]' | sed 's/^.//;s/.$//' | sort | uniq -u
-find "$(cat "$(dirname "$0")/basedir")"    -type f | grep -o   '\[[a-zA-Z0-9_-]\{11\}\]'   | sed 's/^.//;s/.$//' | sort | uniq -u
+find "$(cat "$(dirname "$0")/archivedir")" -type f | grep -o '[[-][a-zA-Z0-9_-]\{11\}[].]' | cut -c 2-12 | sort | uniq
+find "$(cat "$(dirname "$0")/basedir")"    -type f | grep -o   '\[[a-zA-Z0-9_-]\{11\}\]'   | cut -c 2-12 | sort | uniq
 } | sort | uniq -d | \
 while read -r i; do find "$(cat "$(dirname "$0")/archivedir")" -type f -name "*$i*"; done
 # while read -r i; do find ~/Music/ -name "*$i*"; done
