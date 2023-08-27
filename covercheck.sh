7beb342493d4a4a51f28af17be5b2ecb5e5023e4
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-08-27 16:57:48 -0400

    minor

--- covercheck.sh
+++ covercheck.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-find "$(cat "$(dirname "$0")/basedir")" -name '*.m4a' | while read -r i; do
+find "$(cat "$(dirname "$0")/basedir")" -name '*.m4a' ! -empty | while read -r i; do (
   if ffprobe "$i" 2>&1 | grep -q \(attached\ pic\); then
     if   [ -f "$(dirname "$i")/cover.png" ]; then
       echo "redundant: $i"
@@ -9,4 +9,7 @@ find "$(cat "$(dirname "$0")/basedir")" -name '*.m4a' | while read -r i; do
       echo "missing  : $i"
     fi
   fi
+  ) & sleep 0.00625
 done
+# ps;echo ===;wait;ps
+wait
