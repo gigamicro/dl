756f595cba4bda79d504089e644ab7a0f6f851a5
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-09-06 20:45:10 -0400

    make covers carry to archive

--- toarchive.sh
+++ toarchive.sh
@@ -3,4 +3,5 @@ while read -r i; do
   dir="$(cat "$(dirname "$0")/archivedir")/$(basename "$(dirname "$i")")"
   mkdir -vp "$dir"
   mv -v "$i" -t "$dir"
+  cp -vn "$(dirname "$i")/cover."* -t "$dir" 2>/dev/null
 done
