39befa795cadef8dd181b372d6e6166a2214d132
gigamicro <43441570+gigamicro@users.noreply.github.com>
2023-10-18 00:03:45 -0400

    make recentinlog report errors

--- recentinlog.sh
+++ recentinlog.sh
@@ -6,11 +6,15 @@
 sleep 5
 echo "Following $1"
 tail --pid="$1" -n +1 -f /tmp/dl/link/* 2>&1 | \
-grep -e '^\[download] Downloading item [0-9]* of [0-9]*$' -e '^==> .* <==' | \
+grep -e '^\[download] Downloading item [0-9]* of [0-9]*$' -e '^ERROR: ' -e '^==> .* <==' | \
+# grep -v -e 'not available' -e 'only available' | \
 sed 's/^\[download] Downloading item //; ss of s/s; ss^==> .*/s==> s; ss.log <==$s <==s' | \
 while read -r i; do
 	case $i in
 	'==> '*' <==') echo "$i" | head -c -5 |tail -c +5 >/tmp/$$.currentsource ;;
+	*'Video unavailable'*) echo ': Video unavailable' | cat /tmp/$$.currentsource - ;;
+	*'only available to Music Premium'*) echo ': Premium video' | cat /tmp/$$.currentsource - ;;
+	'ERROR: '*) echo ": $i" | cat /tmp/$$.currentsource - ;;
 	*/*) echo ": $i" | cat /tmp/$$.currentsource - ;;
 	 *) echo "err: '$i'" ;;
 	esac
