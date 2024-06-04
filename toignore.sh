f74ad3cec0b856c20251acbf1a6c42291124a1da
gigamicro <gammamicro@gmail.com>
2024-06-04 16:51:32 -0400

    fix toignore.sh being unclear and not providing the correct data

--- toignore.sh
+++ toignore.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 exec 2>&1
-grep -F "$(basename "$(cat "$(dirname "$0")/basedir")")/" | while read -r i; do
-	basename "$i" | "$(dirname "$0")/nametoignores.sh" | \
-	tee -a /dev/fd/2 >>"$(dirname "$0")/ignore/$(basename "$(dirname "$i")").archive" && \
-	echo "	\\-> $(dirname "$i").archive" >>/dev/fd/2
+scriptdir="$(dirname "$0")"
+grep -F "$(basename "$(cat "$scriptdir/basedir")")/" | while read -r i; do
+	dir="$(basename "$(dirname "$i")")"
+	basename "$i" | { printf ./;cat; } | "$scriptdir/nametoignores.sh" | \
+	tee -a /dev/fd/2 >>"$scriptdir/ignore/$dir.archive" && \
+	printf '	\-> %s\n' "$dir.archive" >>/dev/fd/2
 done
