adb464be028d25a900fefb653c78f19c2c3ee7ab
gigamicro <gammamicro@gmail.com>
2024-06-15 00:53:25 -0400

    debuffer recentinlog.sh, add an error and sort errors

--- recentinlog.sh
+++ recentinlog.sh
@@ -5,17 +5,18 @@ while [ "$(printf '%s\n' /tmp/dl/link/* | wc -l)" -lt "$(printf '%s\n' /tmp/dl/l
 	sleep 6
 done
 echo "Following $1 ($(printf '%s\n' /tmp/dl/link/* | wc -l) files)"
-tail "${1+--pid=$1}" -n +1 -f /tmp/dl/link/* 2>&1 | \
-grep -a -e '^\[download] Downloading item [0-9]* of [0-9]*$' -e '^ERROR: ' -e '^==> .* <==' | \
-sed 'ss^\[download] Downloading item \([0-9]*\) of s\1/s;   ss^==> .*/s==> s;  ss.log <==$s <==s' | \
+stdbuf -o L tail "${1+--pid=$1}" -n +1 -f /tmp/dl/link/* 2>&1 | \
+stdbuf -o L grep -a -e '^\[download] Downloading item [0-9]* of [0-9]*$' -e '^ERROR: ' -e '^==> .* <==' | \
+stdbuf -o L sed 'ss^\[download] Downloading item \([0-9]*\) of s\1/s;   ss^==> .*/s==> s;  ss.log <==$s <==s' | \
 while read -r i; do
 	case $i in
 	'==> '*' <==') echo "$i" | head -c -5 |tail -c +5 >/tmp/$$.currentsource ;;
 	*'This video contains content from '*', who has blocked it on copyright grounds'*) echo ': Video copystruck' | cat /tmp/$$.currentsource - ;;
-	*'Video unavailable'*) echo ': Video unavailable' | cat /tmp/$$.currentsource - ;;
-	*'This video is not available'*) echo ': Video not available' | cat /tmp/$$.currentsource - ;;
+	*'[Errno -3] Temporary failure in name resolution'*) echo ": Temporary failure in name resolution" | cat -t /tmp/$$.currentsource - ;;
 	*'only available to Music Premium'*) echo ': Premium video' | cat /tmp/$$.currentsource - ;;
-	'ERROR: '*) echo ": $i" | cat /tmp/$$.currentsource - ;;
+	*'This video is not available'*) echo ': Video not available' | cat /tmp/$$.currentsource - ;;
+	*'Video unavailable'*) echo ': Video unavailable' | cat /tmp/$$.currentsource - ;;
+	'ERROR: '*) echo ": $i" | cat -t /tmp/$$.currentsource - ;;
 	*/*) echo ": $i" | cat /tmp/$$.currentsource - ;;
 	 *) echo "err: '$i'" ;;
 	esac
