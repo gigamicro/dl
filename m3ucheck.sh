9faafe7cd6097d38d850009684e48382e9bacb95
gigamicro <gammamicro@gmail.com>
2024-07-08 13:04:24 -0400

    m3ucheck: remove cat

--- m3ucheck.sh
+++ m3ucheck.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 find "$(cat "$(dirname "$0")/basedir")" -type f ! -name 'cover.*' ! -name '*.m3u' | while read -r i; do
-  cat "$(dirname "$i")"/*.m3u | grep -qF "$(basename "$i")" || echo "$i"
+  grep -qFe "$(basename "$i")" -- "$(dirname "$i")"/*.m3u || printf '%s\n' "$i"
 done
 true
