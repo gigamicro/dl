77524b5d952391e39a5ae5911714bd05fc3a01b0
gigamicro <gammamicro@gmail.com>
2024-07-08 13:05:54 -0400

    tailexisting: don't duplicate lines under 10

--- tailexisting.sh
+++ tailexisting.sh
@@ -2,7 +2,7 @@
 [ -f /tmp/dl.wait.pids ] || exit 0
 while read i; do
 	if kill -0 "$i" 2>/dev/null; then
-		head "/proc/$i/fd/1"
+		head -n -10 "/proc/$i/fd/1" | head
 		echo ...
 		tail --pid="$i" -n 1 -f "/proc/$i/fd/1"
 	fi
