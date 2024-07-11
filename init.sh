80a1c90ddde4aa8073867707bc73e523d352a657
gigamicro <gammamicro@gmail.com>
2024-07-11 13:47:27 -0400

    move archivechecks after toarchives

--- init.sh
+++ init.sh
@@ -25,24 +25,24 @@ echo ===dl\&recentinlog===
 "$scriptdir/dl.sh" & sleep 6;"$scriptdir/recentinlog.sh" $!
 if [ "$1" = "z" ]; then
 	echo ===faVduplicatecheck \| fromfaV===; 		"$scriptdir/faVduplicatecheck.sh" | "$scriptdir/nametoignores.sh" | cut -d\  -f2- | "$scriptdir/fromfaV.sh"
-	echo ===archivecheckstrict \| rm===; 			"$scriptdir/archivecheckstrict.sh" | xargs -rd \\n rm -v --
-	echo ===archivecheck \| rm===; 					"$scriptdir/archivecheck.sh" | xargs -rd \\n rm -v --
-	echo ===archivecheckloose \| rm===; 			"$scriptdir/archivecheckloose.sh" | tee -a "$scriptdir/archivecheckloose.log" | xargs -rd \\n rm -v --
 	echo ===m3ucheck \| toarchive===; 				"$scriptdir/m3ucheck.sh" | "$scriptdir/toarchive.sh"
 	echo ===covercheck\(missing\) \| toarchive===; 	"$scriptdir/covercheck.sh" | grep '^missing' | cut -c 12- | "$scriptdir/toarchive.sh"
 	echo ===squarecheck \| toarchive===; 			"$scriptdir/squarecheck.sh" | "$scriptdir/toarchive.sh"
 	echo ===duplicatecheck \| toarchive===; 		"$scriptdir/duplicatecheck.sh" | "$scriptdir/toarchive.sh"
+	echo ===archivecheckstrict \| rm===; 			"$scriptdir/archivecheckstrict.sh" | xargs -rd \\n rm -v --
+	echo ===archivecheck \| rm===; 					"$scriptdir/archivecheck.sh" | xargs -rd \\n rm -v --
+	echo ===archivecheckloose \| rm===; 			"$scriptdir/archivecheckloose.sh" | tee -a "$scriptdir/archivecheckloose.log" | xargs -rd \\n rm -v --
 	echo ===archiveduplicatecheck \| rm===; 		"$scriptdir/archiveduplicatecheck.sh" | xargs -rd \\n rm -v --
 	echo ===cull===; 								"$scriptdir/cull.sh" "$(cat "$scriptdir/archivedir")"
 else
 	echo ===faVduplicatecheck===; 		"$scriptdir/faVduplicatecheck.sh"
-	echo ===archivecheckstrict===; 		"$scriptdir/archivecheckstrict.sh"
-	echo ===archivecheck===; 			"$scriptdir/archivecheck.sh"
-	echo ===archivecheckloose===; 		"$scriptdir/archivecheckloose.sh"
 	echo ===m3ucheck===; 				"$scriptdir/m3ucheck.sh"
 	echo ===covercheck===; 				"$scriptdir/covercheck.sh"
 	echo ===squarecheck===; 			"$scriptdir/squarecheck.sh"
 	echo ===duplicatecheck===; 			"$scriptdir/duplicatecheck.sh"
+	echo ===archivecheckstrict===; 		"$scriptdir/archivecheckstrict.sh"
+	echo ===archivecheck===; 			"$scriptdir/archivecheck.sh"
+	echo ===archivecheckloose===; 		"$scriptdir/archivecheckloose.sh"
 	echo ===archiveduplicatecheck===; 	"$scriptdir/archiveduplicatecheck.sh"
 fi
 echo ===untouchedcheck===; 	"$scriptdir/untouchedcheck.sh" "$timestamp"
