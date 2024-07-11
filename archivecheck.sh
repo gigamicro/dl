228957f585f1d6452c945250ecca13cb0e70da74
gigamicro <gammamicro@gmail.com>
2024-07-11 18:16:44 -0400

    archivecheck: update regex to be more inclusive

--- archivecheck.sh
+++ archivecheck.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 {
-find "$(cat "$(dirname "$0")/archivedir")" -type f | grep -o '[[-][a-zA-Z0-9_-]\{11\}[].]' | cut -c 2-12 | { [ -z "$1" ] && sort | uniq || cat; }
+find "$(cat "$(dirname "$0")/archivedir")" -type f | grep -oe ' \[[a-zA-Z0-9_-]*\]\.[0-9a-z.]*$' -e '-[a-zA-Z0-9_-]\{11\}\.' |
+ cut -d . -f 1 | sed 's/^[[ -]*//;s/]$//' | { [ -z "$1" ] && sort | uniq || cat; }
 [ -z "$1" ] &&
-find "$(cat "$(dirname "$0")/basedir")"    -type f | grep -o   '\[[a-zA-Z0-9_-]\{11\}\]'   | cut -c 2-12 | sort | uniq
+find "$(cat "$(dirname "$0")/basedir")"    -type f | grep -o  ' \[[a-zA-Z0-9_-]*\]\.[0-9a-z.]*$' | cut -c 3- | cut -d ] -f 1 | sort | uniq
 } | sort | uniq -d | \
-while read -r i; do find "$(cat "$(dirname "$0")/archivedir")" -type f -name "*[[-]${i}[].]*" | { [ -z "$1" ] && cat || tail -n+2; }; done
+while read -r i; do find "$(cat "$(dirname "$0")/archivedir")" -type f -name "* \[$i].*" -o -name "*-$i.*" | { [ -z "$1" ] && cat || tail -n+2; }; done
