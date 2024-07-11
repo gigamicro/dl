7b862f262ddc28e283f319d4b8d925755114c29b
gigamicro <gammamicro@gmail.com>
2024-07-11 15:38:55 -0400

    rework archivecheckloose to blacklist instead of select properties, other tweaks

--- archivecheckloose.sh
+++ archivecheckloose.sh
@@ -9,39 +9,47 @@ find "$basedir"    -type f | grep   '\[[a-zA-Z0-9_-]\{11\}\]'   | grep -o '[^/]*
  sed 's/ \[[a-zA-Z0-9_-]\{11\}\]\..*$//'  | tr '[:lower:]' '[:upper:]' | sort | uniq
 } | sort | uniq -d | \
 "$(dirname "$0")/unpattern.sh" | while read -r i; do
-	# printf "%s\n" "$i" >&2
-	{
-	# [ "${i%/*}" = "Epic Mountain" ] || \
-	# [ "${i%/*}" = "Mad Rat Dead" ] || \
-	# [ "${i%/*}" = "MAD RAT DEAD" ] || \
-	# [ "$i" = "Vertigoaway/MarketingResearch" ] || \
-	# [ "${i%/*}" = "OVERWERK" ] || \
-	[ "$(
-		# find "$(cat "$(dirname "$0")/basedir")/${i%/*}" "$archivedir/${i%/*}" \
-		find \
-		"$(find "$(cat "$(dirname "$0")/basedir"   )/" -type d -iname "${i%/*}")" \
-		"$(find "$archivedir/" -type d -iname "${i%/*}")" \
-		-type f \( -iname "${i#*/} \[*].*" -o -iname "${i#*/}-???????????.*" \) | \
-		{ printf '%s\n' "$i" >&2; cat; } | \
-		# { tee -a /dev/fd/2; } | \
-		xargs -d \\n -n 1 ffprobe -loglevel error -show_entries \
-		'stream_tags=comment,description,synopsis,artist,title,album : stream=duration
-		:format_tags=comment,description,synopsis,artist,title,album : format=duration' | \
-		# ,width,height,sample_rate
-		tr '[:upper:]' '[:lower:]' \
-		|sed \
-		-e 's/:comment=/:synopsis=/;s/:description=/:synopsis=/;' \
-		-e '/:artist=/s/,.*//; /:artist=/s/ feat.*//; /:artist=/s/ - topic$//; /duration=/s/\.[0-9]*$//;' \
-		|grep -v \
-		-e 'youtube.com/watch' \
-		-e ':synopsis=cover (front)'  \
-		-e ':album='  \
-		|\
-		# -e ':artist=bignatesdad' -e ':artist=mad vinnie dead' \
-		# -e '℗ armada springs' -e ':synopsis=provided to youtube by distrokid' \
-		# -e :comment= -e :synopsis= -e :date= -e :handler_name= \
-		sort | uniq -u | tee -a /dev/fd/2 | wc -l
-	)" -eq 0 ] || { printf "%s\n\n" "$i" >&2 ;false;}; } && \
-	find "$archivedir/" -ipath "*/${i} \[*].*" -o -ipath "*/${i}-???????????.*" | { [ -z "$1" ] && cat || sort|tail -n+2; }
-	# echo>&2
-done #>/dev/null #2>/dev/null
+{
+	find "$(cat "$(dirname "$0")/basedir")/" "$archivedir/" -type f \
+	-ipath "*/$i \[*].*" -o -ipath "*/$i-???????????.*" | \
+	# { printf '%s\n' "$i" >&2; cat; } | \
+	xargs -rd \\n -n 1 ffprobe -loglevel error -show_entries 'stream_tags:stream:format_tags:format' | \
+	grep -v \
+	-e '^bit_rate=' \
+	-e '^codec_long_name=' \
+	-e '^codec_name=' \
+	-e '^codec_primaries=' \
+	-e '^codec_tag=' \
+	-e '^codec_tag_string=' \
+	-e '^coded_height=' \
+	-e '^coded_width=' \
+	-e '^color_primaries=' \
+	-e '^display_aspect_ratio=' \
+	-e '^DISPOSITION=' \
+	-e '^extradata_size=' \
+	-e '^filename=' \
+	-e '^format_long_name=' \
+	-e '^format_name=' \
+	-e '^height=' \
+	-e '^id=.x.$' \
+	-e '^initial_padding=' \
+	-e '^nb_frames=' \
+	-e '^profile=' \
+	-e '^sample_aspect_ratio=' \
+	-e '^sample_rate=' \
+	-e '^size=' \
+	-e '^TAG:[a-z]*=Provided to YouTube by ' \
+	-e '^TAG:comment=Cover (front)$' \
+	-e '^TAG:comment=http' \
+	-e '^TAG:compatible_brands=' \
+	-e '^TAG:date=[0-9]*$' \
+	-e '^TAG:ENCODER=' \
+	-e '^TAG:encoder=' \
+	-e '^TAG:handler_name=ISO Media file produced by Google Inc\. Created on: [/0-9]*\.$' \
+	-e '^TAG:language=und$' \
+	-e '^TAG:purl=http' \
+	-e '^TAG:vendor_id=\[0]\[0]\[0]\[0]$' \
+	-e '^width=' \
+	| sort | uniq -u | tee -a /dev/fd/2 | { ! grep '' -m1 >&2;} || { printf %s\\n "$i" | tee -a /dev/fd/2 | tr -c \\n - >&2; false;}
+} && find "$archivedir/" -type f -ipath "*/$i \[*].*" -o -ipath "*/$i-???????????.*" | { [ -z "$1" ] && cat || sort|tail -n+2; } || true
+done
