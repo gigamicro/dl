2ddbdf7fd2e333f113d67401937ea9dd1def5808
gigamicro <gammamicro@gmail.com>
2024-07-17 23:42:17 -0400

    redo m3ucheck

--- m3ucheck.sh
+++ m3ucheck.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-find "$(cat "$(dirname "$0")/basedir")" -type f ! -name 'cover.*' ! -name '*.m3u' | while read -r i; do
-  grep -qFe "$(basename "$i")" -- "$(dirname "$i")"/*.m3u || printf '%s\n' "$i"
+find "$(cat "$(dirname "$0")/basedir")"/ -maxdepth 1 -mindepth 1 -type d | while read -r d; do
+  # printf . >&2
+  if find "$d" -name "*.archive" -print -quit | grep '' -qm 1; then
+    printf 'archive file in %s\n' "$d" >&2
+  else
+    find "$d/." -type f ! -name 'cover.*' ! -name '*.m3u' | grep -vFf "$d"/"${d##*/}".m3u #&& echo "$d" >&2
+  fi
 done
 true
