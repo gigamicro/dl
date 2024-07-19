f658f63e9ada6ba997886145dff03a9734de5533
gigamicro <gammamicro@gmail.com>
2024-07-19 16:20:50 -0400

    archivecheckloose: move sort to C locale, remove duplicated line from grep -m1, add more diff cases

--- archivecheckloose.sh
+++ archivecheckloose.sh
@@ -16,13 +16,54 @@ find "$basedir"    -type f -regex '.* \[[a-zA-Z0-9_-]*\]\.[0-9a-z.]*$' | grep -o
 	# tee -a /dev/fd/2 | \
 	xargs -rd \\n -n 1 ffprobe -loglevel error -show_entries 'stream_tags:stream:format_tags:format' | \
 	sed '
-	/TAG:synopsis=/,/\[\/FORMAT]/{/TAG:synopsis=/b;/\[\/FORMAT]/b; s/^/TAG:synopsis=/};
-	/TAG:description=/,/TAG:synopsis=/{/TAG:description=/b;/TAG:synopsis=/b; s/^/TAG:description=/};
+	/^TAG:comment=/,/^[]/[FORMATSTREAMTAG:synopsisdescription]\{9\}/{
+		/^TAG:comment=/bacomment;
+		/^[]/[FORMATSTREAMTAG:synopdescr]\{9\}/bacomment;
+		s/^/TAG:comment=/
+	};:acomment;
+	/^TAG:description=/,/^[a-zA-Z:_]\{2,\}=/{
+		/^[a-zA-Z:_]\{2,\}=/badescription;
+		s/^/TAG:description=/
+	};:adescription;
+	/^TAG:synopsis=/,/^\[\/FORMAT]$/{
+		/^TAG:synopsis=/basynopsis;
+		/^\[\/FORMAT]$/basynopsis;
+		s/^/TAG:synopsis=/
+	};:asynopsis;
+
+	/TAG:synopsis=/bdates;
+	/TAG:description=/bdates;
+	badates;:dates;
+	s/[0-9][0-9][0-9][0-9] //;Tadates;
+	s/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]//;Tadates;
+	:adates;
+
+	# /TAG:synopsis=/bparens;
+	# /TAG:album=/bparens;
+	# /TAG:description=/bparens;
+	# baparens;:parens;
+	# s/ ([^(]*)$//;Taparens;
+	# :aparens;
+
+	/TAG:synopsis=/bcommas;
+	/TAG:description=/bcommas;
+	bacommas;:commas;
+	s/\([a-z]*\), *\([a-z]*\)/\2 \1/i;Tacommas;
+	s///;Tacommas;
+	:acommas;
+
+	/TAG:synopsis=/bmisc;
+	/TAG:description=/bmisc;
+	bamisc;:misc;
+	s/   */ /g
+	:amisc;
+	/TAG:artist=/s/Mad Vinnie Dead/BigNatesDad/
 	' | \
 	# grep -ve '=' -e '^\[' |
 	# grep -e 'TAG:description' -e 'TAG:synopsis' |
 	# tee -a /dev/fd/2 |
 	grep -v \
+	-e '^$' \
 	-e '^bit_rate=' \
 	-e '^codec_long_name=' \
 	-e '^codec_name=' \
@@ -33,8 +74,8 @@ find "$basedir"    -type f -regex '.* \[[a-zA-Z0-9_-]*\]\.[0-9a-z.]*$' | grep -o
 	-e '^coded_width=' \
 	-e '^color_primaries=' \
 	-e '^display_aspect_ratio=' \
-	-e '^DISPOSITION=' \
 	-e '^DISPOSITION:[a-z]*=' \
+	-e '^DISPOSITION=' \
 	-e '^extradata_size=' \
 	-e '^filename=' \
 	-e '^format_long_name=' \
@@ -52,14 +93,18 @@ find "$basedir"    -type f -regex '.* \[[a-zA-Z0-9_-]*\]\.[0-9a-z.]*$' | grep -o
 	-e '^TAG:comment=http' \
 	-e '^TAG:compatible_brands=' \
 	-e '^TAG:date=[0-9]*$' \
+	-e '^TAG:description=[Pp]laylist: http' \
+	-e '^TAG:description=http' \
 	-e '^TAG:ENCODER=' \
 	-e '^TAG:encoder=' \
-	-e '^TAG:handler_name=ISO Media file produced by Google Inc\. Created on: [/0-9]*\.$' \
+	-e '^TAG:handler_name=ISO Media file produced by Google Inc\.' \
 	-e '^TAG:language=und$' \
 	-e '^TAG:purl=http' \
+	-e '^TAG:synopsis=[Pp]laylist: http' \
+	-e '^TAG:synopsis=http' \
 	-e '^TAG:vendor_id=\[0]\[0]\[0]\[0]$' \
 	-e '^width=' \
-	| sort | uniq -u | tee -a /dev/fd/2 | { ! grep '' -m1 >&2;} || { printf %s\\n "$i" | tee -a /dev/fd/2 | tr -c \\n - >&2; false;}
+	| LC_ALL=C sort | uniq -ui | tee -a /dev/fd/2 | { ! grep '' -qm1;} || { printf %s\\n "$i" | tee -a /dev/fd/2 | tr -c \\n - >&2; false;}
 } && find "$archivedir/" -type f -ipath "*/$i \[*].*" -o -ipath "*/$i-???????????.*" | { [ -z "$1" ] && cat || sort|tail -n+2; } || true
 done
 
