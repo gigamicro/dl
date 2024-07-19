5456aa7bb7830b59f4a9b4fe698e61a361613f9b
gigamicro <gammamicro@gmail.com>
2024-07-19 17:36:54 -0400

    fix m3ucheck giving extra /. in output

--- m3ucheck.sh
+++ m3ucheck.sh
@@ -4,7 +4,7 @@ find "$(cat "$(dirname "$0")/basedir")"/ -maxdepth 1 -mindepth 1 -type d | while
   if find "$d" -name "*.archive" -print -quit | grep '' -qm 1; then
     printf 'archive file in %s\n' "$d" >&2
   else
-    find "$d/." -type f ! -name 'cover.*' ! -name '*.m3u' | grep -vFf "$d"/"${d##*/}".m3u #&& echo "$d" >&2
+    find "$d/." -type f ! -name 'cover.*' ! -name '*.m3u' | grep -vFf "$d"/"${d##*/}".m3u | sed 'ss/\./s/s'
   fi
 done
 true
