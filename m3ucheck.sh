9f904f4826470f08a4466e57b725ee68a0279478
gigamicro <gammamicro@gmail.com>
2024-08-02 22:14:31 -0400

    m3ucheck: fix literally not working

--- m3ucheck.sh
+++ m3ucheck.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-find "$(cat "$(dirname "$0")/basedir")"/ -maxdepth 1 -mindepth 1 -type d | while read -r d; do
-  # printf . >&2
+find "$(cat "$(dirname "$0")/basedir")"/ -maxdepth 1 -mindepth 1 -type d | 
+while read -r d; do
   if find "$d" -name "*.archive" -print -quit | grep '' -qm 1; then
     printf 'archive file in %s\n' "$d" >&2
   else
-    find "$d/." -type f ! -name 'cover.*' ! -name '*.m3u' | grep -vFf "$d"/"${d##*/}".m3u | sed 'ss/\./s/s'
+    mmmu="${d}/${d##*/}.m3u"
+    [ -f "$mmmu" ] ||{ printf 'no m3u in %s\n' "$d" >&2;continue;}
+    cd "$d" || continue
+    find . -type f ! -name 'cover.*' ! -name '*.m3u' -print0 | grep -zvFxf "$mmmu" | xargs -0rn1 printf '%s/%s\n' "$d" | sed 'ss/\./s/s'
   fi
 done
 true
