68cc484d80bacaf6fafca65bc9e3fe55786c27ed
gigamicro <gammamicro@gmail.com>
2024-08-13 15:06:42 -0400

    duplicatecheck: use -z

--- duplicatecheck.sh
+++ duplicatecheck.sh
@@ -2,4 +2,4 @@
 find "$(cat "$(dirname "$0")/basedir")" -type f | \
 sed 'ss[^/]* \[s\ts' | \
 sort | uniq -d | \
-while read -r i; do find "${i%	*}" -type f -name "* \[${i##*	}" | xargs -rd \\n stat -c '%Z %n' | sort -r | tail -n +2 | cut -c 12-; done
+while read -r i; do find "${i%	*}" -type f -name "* \[${i##*	}" -print0 | xargs -0r stat -c '%Z %n' | sort -r | tail -n +2 | cut -c 12-; done
