9cf2258be5839d96263c9cb0f1dd79839d321c81
gigamicro <gammamicro@gmail.com>
2024-09-09 22:42:33 -0400

    archivecheckstrict: passthrough args

--- archivecheckstrict.sh
+++ archivecheckstrict.sh
@@ -2,4 +2,4 @@
 {
 find "$(cat "$(dirname "$0")/archivedir")" -type f ! -name 'cover.*' ! -name '*.m3u' -print0 | xargs -0 basename -za | sort -z | uniq -z
 find "$(cat "$(dirname "$0")/basedir")"    -type f ! -name 'cover.*' ! -name '*.m3u' -print0 | xargs -0 basename -za | sort -z | uniq -z
-} | sort -z | uniq -zd | "$(dirname "$0")/unpattern.sh" | xargs -0rn 1 find "$(cat "$(dirname "$0")/archivedir")" -name
+} | sort -z | uniq -zd | "$(dirname "$0")/unpattern.sh" -z | xargs -0rn 1 find "$(cat "$(dirname "$0")/archivedir")" -name
