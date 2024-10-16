6716785c91d038253fc04bd0840d29d706b34ef9
gigamicro <gammamicro@gmail.com>
2024-10-16 15:32:05 -0400

    multiline tolists' sed

--- tolists.sh
+++ tolists.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-sed 's/www\.//; s/music\.//; s/watch?v=[^&]*&/playlist?/; s/index=[^&]*//; a\' >> "$(dirname "$0")/$1.m3u"
+sed '
+s/www\.//;
+/youtube\.com/{ s/music\.//; s/watch?v=[^&]*&/playlist?/; s/index=[^&]*&//; }
+# /bandcamp\.com/{ s///; }
+/soundcloud\.com/{ s/\/tracks$//; }
+a\' >> "$(dirname "$0")/$1.m3u"
